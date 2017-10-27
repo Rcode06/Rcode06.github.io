c4fcc417dcddf2173e0f4d3c991b56335a1b5368
reyna <reyna@reynas-MacBook-Pro.local>
2017-10-26 21:22:30 -0700

    first commit

--- server.js
+++ server.js
@@ -1,53 +1,33 @@
+
+
+
 // Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-// Sets up the Express App
-// =============================================================
+var port = process.env.PORT || 3000;
+
 var app = express();
-var PORT = 3000;
-
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
+app.use(express.static(process.cwd() + '/public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//  POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./app/public/home.html"));
-});
+// Set Handlebars 
+var exphbs = require('express-handlebars');
 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-});
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
+// Import routes 
+var routes = require('./controllers/burgers_controller.js');
 
+app.use('/', routes);
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
-// app.use('/static', express.static(path.join(__dirname, 'app/public')))
-
-app.use(express.static('app'));
-
-require('./app/routes/apiRoute.js')(app); 
-require('./app/routes/htmlRoute.js')(app);
-
-
-
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(process.env.PORT || PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-// app.listen(process.env.PORT || 3000;
+app.listen(port);
